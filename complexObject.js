19a89d98177c77a6230a2332d506335b674b7636
Advitya <advityasharma21@gmail.com>
2020-05-10 19:24:34 +0530

    collection

---
+++ complexObject.js
@@ -0,0 +1,21 @@
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist":"Eminem",
+      "title":"Slim Shady EP",
+      "release_year":2018,
+      "formats":["Digital","CD"]
+    }
+    // Add a record here
+  ];
+  
