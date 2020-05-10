1fae6d78125f15e8cfcc7a73a37aad055d4289f1
Advitya <advityasharma21@gmail.com>
2020-05-10 17:45:32 +0530

    objects

---
+++ jsObjects.js
@@ -0,0 +1,9 @@
+var myDog = {
+    // Only change code below this line
+    name : "drago",
+    legs : 4,
+    tails : 1,
+    friends : ["POKO","NIKO","RIKO"]
+    // Only change code above this line
+    };
+    
