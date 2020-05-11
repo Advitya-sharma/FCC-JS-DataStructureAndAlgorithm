fd166dd5f94aa5182d5fdbd809c075954bc66d10
Advitya <advityasharma21@gmail.com>
2020-05-11 13:42:29 +0530

    destructing assignment

--- ES6/defaultParam.js
+++ ES6/defaultParam.js
@@ -1,3 +1,3 @@
 // Only change code below this line
-const increment = (number=1, value) => number + value;
+const increment = (number, value=1) => number + value;
 // Only change code above this line
